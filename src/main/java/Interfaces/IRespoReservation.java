11e070e89ca973d0c0cb55b44b9cac274b726634
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-03-03 10:41:25 +0100

    Commit

--- src/main/java/Interfaces/IRespoReservation.java
+++ src/main/java/Interfaces/IRespoReservation.java
@@ -1,6 +1,12 @@
 package Interfaces;
 
 public interface IRespoReservation {
+	void showReservations();
+	boolean addReservation(Integer ID, IClient cliente, IProduct product);
+	boolean delReservation(Integer ID);
+	void modifyFechaCreacion(Integer ID, String date);
+	void modifyFechaFinal(Integer ID, String date);
+	void modifyStatus(Integer ID, Status status);
     void saveFile(String url);
     void loadFile(String url);
 }
