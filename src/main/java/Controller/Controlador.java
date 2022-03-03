124bd5d541cddf4424152fa0352454966560af96
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-03-03 11:29:40 +0100

    Revert "No liarla mas"
    This reverts commit b7047e31e43512c3da651da5567ee4cccd5fe4f6.

--- src/main/java/Controller/Controlador.java
+++ src/main/java/Controller/Controlador.java
@@ -1,22 +1,9 @@
 package Controller;
 
-import Interfaces.IClient;
 import Interfaces.IController;
-import Interfaces.IProduct;
-import Interfaces.IRepoProduct;
-import Interfaces.IReposClient;
-import Interfaces.IReservation;
-import Interfaces.IRespoReservation;
-import Interfaces.IVista;
 
 public class Controlador implements IController{
-	IVista vista;
-	IReposClient RepoClient;
-	IClient client;
-	IProduct product;
-	IRepoProduct RepoProduct;
-	IReservation reserva;
-	IRespoReservation RepoReserva;
+	
 	
 	
 	@Override
