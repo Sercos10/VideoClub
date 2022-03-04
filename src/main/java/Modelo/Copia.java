e0c3092863452b25fc831c1312e643e11cab240b
unknown <antonioajlb98@gmail.com>
2022-03-04 09:56:09 +0100

    Producto y copia

---
+++ src/main/java/Modelo/Copia.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+import Interfaces.ICopia;
+
+public class Copia implements ICopia{
+	
+	private Integer id;
+	private String name;
+
+	public Integer getID() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/main/java/Modelo/RepoProduct.java
@@ -0,0 +1,111 @@
+package Modelo;
+
+import java.io.File;
+import java.util.HashMap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBContextFactory;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import Enums.Category;
+import Interfaces.IProduct;
+import Interfaces.IRepoProduct;
+
+@XmlRootElement(name = "RepoProduct")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class RepoProduct implements IRepoProduct {
+	private HashMap<Integer, IProduct> productList;
+	private static RepoProduct _instance;
+	
+	private RepoProduct() {
+		productList = new HashMap<Integer, IProduct>();
+	}
+
+	public void showProductList() {
+	}
+
+	public static RepoProduct getInstance() {
+		if (_instance == null) {
+			_instance = new RepoProduct();
+		}
+		return _instance;
+	}
+
+	public boolean addProduct(IProduct p) {
+		boolean added = false;
+		if (p != null) {
+			if (!productList.containsKey(p.getID())) {
+				productList.put(p.getID(), p);
+				added = true;
+			}
+		}
+		return added;
+	}
+
+	public boolean removeProduct(IProduct p) {
+		boolean removed = false;
+		if (p != null) {
+			if (productList.containsKey(p.getID())) {
+				productList.remove(p.getID());
+				removed = true;
+			}
+		}
+		return removed;
+	}
+
+	public void modifyName(Integer ID, String name) {
+		if (productList.containsKey(ID)) {
+			productList.get(ID).setName(name);
+		}
+	}
+
+	public void modifyDesc(Integer ID, String desc) {
+		if (productList.containsKey(ID)) {
+			productList.get(ID).setDescription(desc);
+		}
+	}
+
+	public void modifyPrize(Integer ID, float prize) {
+		if (productList.containsKey(ID)) {
+			productList.get(ID).setPrice(prize);
+		}
+	}
+
+	public void saveFile(String url) {
+		JAXBContextFactory contexto;
+		try {
+			contexto = (JAXBContextFactory) JAXBContext.newInstance(RepoProduct.class);
+			Marshaller m = ((JAXBContext) contexto).createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			m.marshal(_instance, new File(url));
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadFile(String url) {
+		JAXBContext contexto;
+
+		try {
+			contexto = JAXBContext.newInstance(RepoProduct.class);
+			Unmarshaller um = contexto.createUnmarshaller();
+			RepoProduct newClub = (RepoProduct) um.unmarshal(new File(url));
+			productList = newClub.productList;
+			;
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void modifyCategory(Integer ID, Category category) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
