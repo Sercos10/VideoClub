ff07f54e80be5c960d37ac505eec24f86a4e68a0
Sercos10 <sergiocosano98@gmail.com>
2022-03-10 10:58:04 +0100

    antonio pesao

--- src/main/java/Interfaces/IProduct.java
+++ src/main/java/Interfaces/IProduct.java
@@ -1,14 +1,20 @@
 package Interfaces;
 
+import Enums.Category;
+
 public interface IProduct {
     Float getPrice();
     String getDescription();
     Integer getID();
     String getName();
+    Integer getNCopias();
+    Category getCategory();
     void setName(String name);
     void setPrice(Float Price);
     void setDescription(String Description);
     void setID(Integer ID);
+    void setNCopias(Integer ncopias);
+    void setCategory(Category categoria);
     boolean equals(Object o);
     String toString();
 }
