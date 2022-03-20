3ef0ea86f686137da0c8f250e8930766e9d6fad0
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2022-03-20 18:07:30 +0100

    Comentando codigo

--- src/main/java/Enums/Status.java
+++ src/main/java/Enums/Status.java
@@ -1,5 +1,10 @@
 package Enums;
 
 public enum Status {
+	/**
+	 * 3 tipos de reservas para diferenciar el estado, si una reserva esta en reservado no puede aparecer
+	 * para reservarla otra vez, si esta expirada tampoco puede aparecer, si esta entregada si puede aparecer
+	 * para volverla a reservar. Por defecto, al crear una reserva se pone en reservado.
+	 */
 	RESERVADO, EXPIRADO, ENTREGADO
 }
